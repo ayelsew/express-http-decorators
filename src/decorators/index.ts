7e28a48122f9189b85110126ea190bae78a00497
ayelsew <wesley.waaraujo@gmail.com>
2022-01-02 01:38:33 -0300

    update metods

--- src/decorators/index.ts
+++ src/decorators/index.ts
@@ -1,11 +1,11 @@
 import { Method, MethodParameters } from '../declarations';
 import { HTTPFactory } from './HTTPFactory';
 
-export const Post = (methodParameters?: MethodParameters) => HTTPFactory(Method.post, methodParameters);
-export const Patch = (methodParameters?: MethodParameters) => HTTPFactory(Method.patch, methodParameters);
-export const Delete = (methodParameters?: MethodParameters) => HTTPFactory(Method.delete, methodParameters);
+export function Post(methodParameters?: MethodParameters) { return HTTPFactory(Method.post, methodParameters); }
+export function Patch(methodParameters?: MethodParameters) { return HTTPFactory(Method.patch, methodParameters); }
+export function Delete(methodParameters?: MethodParameters) { return HTTPFactory(Method.delete, methodParameters); }
 export function Get(methodParameters?: MethodParameters) { return HTTPFactory(Method.get, methodParameters); }
-export const Put = (methodParameters?: MethodParameters) => HTTPFactory(Method.put, methodParameters);
+export function Put(methodParameters?: MethodParameters) { return HTTPFactory(Method.put, methodParameters); }
 
 export * from './factory';
 export * from './Controller';
